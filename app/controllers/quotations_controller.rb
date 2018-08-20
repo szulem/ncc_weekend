28958c74c9a8927b905a21f0aeb8b69c037cb951
Mirek <mirek@szul.eu>
2018-08-20 19:54:47 +0200

    12 quotations per page.

--- app/controllers/quotations_controller.rb
+++ app/controllers/quotations_controller.rb
@@ -1,6 +1,6 @@
 class QuotationsController < ApplicationController
   def index
-  	@quotes = Quotation.all.paginate(:page => params[:page], :per_page => 2)
+  	@quotes = Quotation.all.paginate(:page => params[:page], :per_page => 12)
   end
 
   def show
