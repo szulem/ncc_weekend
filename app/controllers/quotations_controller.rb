53aa71279f1f09991397c649176a5a57e3d3e3bc
Mirek <mirek@szul.eu>
2018-09-11 14:33:14 +0200

    Display errors if form is invalid

--- app/controllers/quotations_controller.rb
+++ app/controllers/quotations_controller.rb
@@ -1,32 +1,31 @@
 class QuotationsController < ApplicationController
   def index
-  	@quotes = Quotation.all.paginate(:page => params[:page], :per_page => 12).order("updated_at DESC")
+    @quotes = Quotation.all.paginate(:page => params[:page], :per_page => 12).order("updated_at DESC")
   end
 
   def show
-  	
+    
   end
 
   def new
-  	@quote = Quotation.new
+    @quote = Quotation.new
   end
 
   def create
-  	@quote = Quotation.new(quotations_params)
-  	if @quote.save
-  		flash[:success] = "You have added a new quotation!"
-  		redirect_to root_path
-  	else
-  		flash[:danger] = "You haven't entered any text or the text you entered has less than 10 characters."
-  		redirect_to new_quotation_path
-  	end
+    @quote = Quotation.new(quotations_params)
+    if @quote.save
+      flash[:success] = "You have added a new quotation!"
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
 
-	private
+  private
 
-	def quotations_params
-		params.require(:quotation).permit(:content)
-	end
+  def quotations_params
+    params.require(:quotation).permit(:content)
+  end
 
 end
